e5dd3c53291969dcc568534df2a6de58cf977dba
jwy4285 <jwy4285@naver.com>
2023-08-25 11:36:47 +0900

    왜 안되지?

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,17 +1,8 @@
 package shop.mtcoding.blogv2.board;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
-
-    @GetMapping("/user/updateForm")
-    public String updateForm() {
-     
-        return "user/updateForm";
-    }
-
 
 }
