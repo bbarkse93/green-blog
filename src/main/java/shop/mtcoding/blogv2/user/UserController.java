f0358fc1dd99e3f652dcf7b566551f46bd91c113
saki8661 <saki8661@naver.com>
2023-08-25 11:15:47 +0900

    로그인 기능 구현

--- src/main/java/shop/mtcoding/blogv2/user/UserController.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserController.java
@@ -1,8 +1,20 @@
 package shop.mtcoding.blogv2.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    } 
+
+    @PostMapping("/login")
+    public String login() {
+        return "redirect:/";
+    }
 
 }
