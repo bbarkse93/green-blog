7b274ab5d7174d8222ae4de0e090012d0d6095cf
jwy4285 <jwy4285@naver.com>
2023-08-25 12:22:22 +0900

    출력하다가 집에 감

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -2,6 +2,7 @@ package shop.mtcoding.blogv2.board;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -9,18 +10,20 @@ public class BoardController {
 
     @GetMapping("/user/updateForm")
     public String updateForm() {
-     
+     System.out.println("hello");
         return "user/updateForm";
     }
-
-    @PostMapping("/board/{id}/delete")
-    public  String dgelete() {
-        
-    return ("/");
+  
+    @GetMapping("/board/{id}")
+    public String detail() {
+    
+        return "board/detail";
     }
-    @GetMapping("/user")
-    public String user(){
-        return "user";
+  
+    @PostMapping("/board/{id}/delete")
+    public  String delete() {
+System.out.println("aslkdsgn");
+    return ("/");
     }
 
 }
