43d7d9717fc9fec6b717a1020f31a0cd22c16b33
jwy4285 <jwy4285@naver.com>
2023-08-25 11:17:17 +0900

    회원정보보기

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,8 +1,17 @@
 package shop.mtcoding.blogv2.board;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
+
+    @GetMapping("/user/updateForm")
+    public String updateForm() {
+     
+        return "user/updateForm";
+    }
+
 
 }
